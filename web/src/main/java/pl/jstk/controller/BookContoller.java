3b817e774f32f2c7ed8d3253902969b54ed4659e
Pawel Kaptur <pawel.kaptur@protonmail.com>
2018-07-30 13:46:27 +0200

    adding book form is showing

--- web/src/main/java/pl/jstk/controller/BookContoller.java
+++ web/src/main/java/pl/jstk/controller/BookContoller.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pl.jstk.entity.BookEntity;
+import pl.jstk.enumerations.BookStatus;
+import pl.jstk.mapper.BookMapper;
 import pl.jstk.service.BookService;
+import pl.jstk.to.BookTo;
 
 
 @Controller
@@ -27,5 +31,21 @@ public class BookContoller {
     public String getBookById(@RequestParam("id") Long bookId, Model model) {
         model.addAttribute("book", bookService.findBookbyId(bookId));
         return "book";
+    }
+
+    @GetMapping("/books/add")
+    public String addBook(Model model){
+        model.addAttribute("newBook", new BookTo());
+        return "addBook";
+    }
+
+    @PostMapping("/greeting")
+    public String addBook(@ModelAttribute("newBook") BookTo book, String title, String authors, BookStatus bookStatus){
+        book.setId(213L);
+        book.setTitle(title);
+        book.setAuthors(authors);
+        book.setStatus(bookStatus);
+        bookService.saveBook(book);
+        return "welcome";
     }
 }
